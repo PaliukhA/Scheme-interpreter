a208d2f4a518734b961b8d35378b3ab716360c32
Anton Paliukhovich <antonpaliukh@yandex.by>
2020-05-18 16:56:34 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -9,8 +9,55 @@ int main() {
     std::string line;
 //    std::ifstream in1;
 //    in1.open("input.txt");
-    while (getline(std::cin , line)) {
-        std::cout << proc.Build(line) << "\n";
+    std::string type;
+    getline(std::cin, type);
+    if (type == "monocode") {
+        while (getline(std::cin, line)) {
+            std::cout << proc.Build(line) << "\n";
+        }
+    } else if (type == "splitcode") {
+        int balance = 0;
+        std::string all_line;
+        while (getline(std::cin, line)) {
+            bool is_quotes = true;
+            for (char x: line) {
+                if(x != '\'') {
+                    is_quotes = false;
+                }
+                if (x == '(') {
+                    balance++;
+                } else if (x == ')') {
+                    balance--;
+                }
+                all_line += x;
+            }
+            if (balance == 0 && !is_quotes) {
+                std::cout << proc.Build(all_line) << "\n";
+                all_line  = "";
+            }
+        }
+    } else if (type == "file") {
+        std::freopen("input.txt","r", stdin);
+        int balance = 0;
+        std::string all_line;
+        while (getline(std::cin, line)) {
+            bool is_quotes = true;
+            for (char x: line) {
+                if(x != '\'') {
+                    is_quotes = false;
+                }
+                if (x == '(') {
+                    balance++;
+                } else if (x == ')') {
+                    balance--;
+                }
+                all_line += x;
+            }
+            if (balance == 0 && !is_quotes) {
+                std::cout << proc.Build(all_line) << "\n";
+                all_line  = "";
+            }
+        }
     }
     return 0;
 }
